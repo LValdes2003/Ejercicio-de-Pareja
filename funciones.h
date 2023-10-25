c41fbe2392fbcded60e18e1d509c1148915a5dfd
LValdes2003 <lvaldesp@myuax.com>
2023-10-25 19:08:00 +0200

    funciones y prototipos

---
+++ funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_Y_PROTOTIPOS_FUNCIONES_H
+#define FUNCIONES_Y_PROTOTIPOS_FUNCIONES_H
+
+int suma(int a, int b, int c);
+void imprimir();
+int sumaVariable(int n, ...);
+double porcentage(double a, int b = 100);
+int fibonacci(int n);
+
+#endif //FUNCIONES_Y_PROTOTIPOS_FUNCIONES_H
--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
-//
-// Created by Oblitionmaster on 19/10/2023.
-//
-
 #include "iostream"
-#include "string"
+#include "funciones.h"
 
-int main() {
-    int edad;
-    std::string nombre{};
+// Funciones en línea
+inline int suma(int a, int b) {
+    return a + b;
+}
 
-    std::cout << "Introduce su nombre" << std::endl;
-    std::cin >> nombre;
+extern void juego();
 
-    std::cout << "Introduce su edad" << std::endl;
-    std::cin >> edad;
-
-    std::cout << "Hola " << nombre << " tienes " << edad << " años" << std::endl;
+// función main
+int main(int argc, char** argv) {
+    // Llamada de las funciones
+    // Declaración de una función
+    imprimir();
+    std::cout << "La posición 7 en la secuencia de fibonacci es " << fibonacci(7) << std::endl;
+    std::cout << "El valor porcentual de 10 es " << porcentage(10) << "%" << std::endl;
+    std::cout << "La suma de 2 y 3 es " << suma(2, 3) << std::endl;
+    std::cout << "La suma de 2, 3 y 4 es " << suma(2, 3, 4) << std::endl;
+    std::cout << "La suma de 2, 3, 4 y 5 es " << sumaVariable(4, 2, 3, 4, 5) << std::endl;
+    juego();
+    std::cout << "El valor de argc es " << argc << std::endl;
+    for (int i = 0; i < argc; ++i) {
+        std::cout << argv[i] << std::endl;
+    }
     return 0;
 }
